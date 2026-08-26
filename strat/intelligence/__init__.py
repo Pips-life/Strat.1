e8a6b08f560c5dafe7ec3cb8af85a7eab15c802c
Pips-life <piplife1@gmail.com>
2026-08-27 02:37:24 +0300

    Export canonical QOF precision planner

--- strat/intelligence/__init__.py
+++ strat/intelligence/__init__.py
@@ -1,8 +1,10 @@
 """Provider-neutral intelligence adapters for Strategy 001."""
 from .greeks import GreekSignal, delta_pressure, gamma_exposure, gamma_regime, iv_regime
 from .momentum import MomentumSignal, velocity_signal
+from .precision import PrecisionEntryPlanner, PrecisionPlan
 
 __all__ = [
     "GreekSignal", "delta_pressure", "gamma_exposure", "gamma_regime", "iv_regime",
     "MomentumSignal", "velocity_signal",
+    "PrecisionEntryPlanner", "PrecisionPlan",
 ]
