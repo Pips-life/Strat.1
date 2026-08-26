7974b0ab5eb8715b796136f1d872b8e8f97a2f06
Pips-life <piplife1@gmail.com>
2026-08-27 02:46:28 +0300

    Export multi-timeframe QOF precision planner

--- strat/intelligence/__init__.py
+++ strat/intelligence/__init__.py
@@ -1,10 +1,16 @@
 """Provider-neutral intelligence adapters for Strategy 001."""
 from .greeks import GreekSignal, delta_pressure, gamma_exposure, gamma_regime, iv_regime
 from .momentum import MomentumSignal, velocity_signal
-from .precision import PrecisionEntryPlanner, PrecisionPlan
+from .precision import (
+    MultiTimeframePrecisionPlan,
+    MultiTimeframePrecisionPlanner,
+    PrecisionEntryPlanner,
+    PrecisionPlan,
+)
 
 __all__ = [
     "GreekSignal", "delta_pressure", "gamma_exposure", "gamma_regime", "iv_regime",
     "MomentumSignal", "velocity_signal",
     "PrecisionEntryPlanner", "PrecisionPlan",
+    "MultiTimeframePrecisionPlanner", "MultiTimeframePrecisionPlan",
 ]
