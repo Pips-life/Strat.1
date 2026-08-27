fff06da687b7caac0cce97c687b94678fe969925
Pips-life <piplife1@gmail.com>
2026-08-27 11:12:32 +0300

    Expose volatility Greeks intelligence

--- strat/intelligence/__init__.py
+++ strat/intelligence/__init__.py
@@ -7,9 +7,11 @@ from .precision import (
     PrecisionEntryPlanner,
     PrecisionPlan,
 )
+from .volatility import VolatilitySnapshot, build_volatility_snapshot
 
 __all__ = [
     "GreekSignal", "delta_pressure", "gamma_exposure", "gamma_regime", "iv_regime",
+    "VolatilitySnapshot", "build_volatility_snapshot",
     "MomentumSignal", "velocity_signal",
     "PrecisionEntryPlanner", "PrecisionPlan",
     "MultiTimeframePrecisionPlanner", "MultiTimeframePrecisionPlan",
