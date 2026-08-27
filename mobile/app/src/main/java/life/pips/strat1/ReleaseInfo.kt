ae126c91ce59af1ed575e84bca7f2bc7b5a5f995
Pips-life <piplife1@gmail.com>
2026-08-27 16:36:46 +0300

    chore(mobile): remove duplicate release model

--- mobile/app/src/main/java/life/pips/strat1/ReleaseInfo.kt
+++
@@ -1,9 +0,0 @@
-package life.pips.strat1
-
-data class ReleaseInfo(
-    val versionName: String,
-    val versionCode: Int,
-    val releaseName: String,
-    val notes: String,
-    val apkUrl: String
-)
