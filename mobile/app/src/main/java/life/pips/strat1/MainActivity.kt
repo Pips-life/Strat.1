0f97e21aa81e34532f5b07de40cb51a79058809d
Pips-life <piplife1@gmail.com>
2026-09-26 19:08:24 +0300

    Fix S006 zone map color conversion import

--- mobile/app/src/main/java/life/pips/strat1/MainActivity.kt
+++ mobile/app/src/main/java/life/pips/strat1/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
